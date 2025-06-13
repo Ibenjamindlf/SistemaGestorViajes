a0c4bb3802327cc2ac040f4b330456d719c90b47
Benjamin de la fuente <206235917+Ibenjamindlf@users.noreply.github.com>
2025-06-13 16:24:15 -0300

    Benja

---
+++ Pasajero.php
@@ -0,0 +1,9 @@
+<?php
+// De los pasajeros se conoce su nombre, apellido, número de documento y teléfono.
+class Pasajero{
+    // Zona de atributos 
+    private ;$
+    private ;$
+    private ;$
+}
+?>

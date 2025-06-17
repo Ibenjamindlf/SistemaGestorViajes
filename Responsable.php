7faebc84fbefbd93b970651885c234acdafa4c78
Benjamin de la fuente <206235917+Ibenjamindlf@users.noreply.github.com>
2025-06-17 17:02:19 -0300

    responsable.php en desarrollo vero

--- Responsable.php
+++ Responsable.php
@@ -1,5 +1,5 @@
 <?php
-// Responsable.php inicio
+// Del responsable se conoce el numero de empleado, el numero de licencia, su nomre y su apellido
 Class Responsable {
     // Atributo estático (id autoincrementado)
     private static int $numeroEmpleadoStatic = 0;
@@ -20,7 +20,6 @@ Class Responsable {
         $this->nombre = $nombre;
         $this->apellido = $apellido;
     }
-
     // GETTERS
     public function getNumeroEmpleado(){
         return $this->numeroEmpleado;
@@ -37,7 +36,6 @@ Class Responsable {
     public function getApellido(){
         return $this->apellido;
     }
-
     // SETTERS
     public function setNumeroLicencia($numeroLicencia){
         $this->numeroLicencia = $numeroLicencia;
@@ -65,5 +63,91 @@ Class Responsable {
         ;
     }
     // Realizar las 5 funciones (buscar,listar,insertar,modificar,eliminar) -> SQL phpMyAdmin
+
+    // Funcion para buscar un Responsable por su numero de empleado en la base de datos
+    // Return true si se encontró al responsable, false si no
+    public function buscar($numEmpleado) {
+        $dataBase = new DataBase();
+        $consulta = "SELECT * FROM responsale WHERE numeroEmpleado = '". $numEmpleado . "'";
+        $respuesta = false;
+
+        if ($dataBase->iniciar()) {
+            if($dataBase->ejecutar($consulta)) {
+                if($fila = $dataBase->registro()) {
+                /// CONSULTAR SI TENGO QUE HACER LO MISMO CON EL ATRIBUTO ESTATICO
+                    $this->setNumeroLicencia($fila['numeroLicencia']);
+                    $this->setNombre($fila['nombre']);
+                    $this->setApellido($fila['apellido']);
+
+                    $respuesta = true;
+                }
+            } else {
+                throw new Exception("Error: la consulta no se pudo ejecutar");
+            }
+        } else {
+        throw new Exception("Error: la base de datos no se pudo iniciar");
+        }
+
+        return $respuesta;
+    }
+    // Funcion para listar toda la tabla responsable
+    // Return el arreglo con los responsables o null
+    public static function listar($condicion = "") {
+        $arrayResponsable = null;
+        $dataBase = new DataBase();
+        $consulta = "SELECT * FROM responsable";
+
+        if ($condicion != "") {
+            $consulta .= " WHERE " . $condicion;
+        }
+        $consulta .= " ORDER BY nombre";
+
+        if ($dataBase->iniciar()) {
+            if ($dataBase->ejercutar($consulta)) {
+                $arrayResponsable = [];
+
+                while ($row = $dataBase->registro()) {
+                    $objResponsable = new Responsable(
+                        $row['numeroEmpleado'],
+                        $row['numeroLicencia'],
+                        $row['nombre'],
+                        $row['apellido']
+                    );
+                    $arrayResponsable[] = $objResponsable;
+                }
+            } else {
+                throw new Exception("Error: la consulta no se pudo ejecutar");
+            }
+        } else {
+            throw new Exception("Error: la base de datos no se pudo iniciar");
+        }
+        return $arrayResponsable;
+    }
+    // Funcion para insertar un nuevo responsable
+    // Return true si se logro insertar el responsable nuevo, si no false
+    public function insertar() {
+        $dataBase = new DataBase();
+        $respuesta = false;
+        $consulta = "INSERT INTO responsable(numeroEmpleado, numeroLIcencia, nombre, apellido)
+                    VALUES ('".$this->getNumeroEmpleado()."','".$this->getNumeroLicencia()."','".$this->getNombre()."','".$this->getApellido()."')";
+        if ($dataBase->iniciar()) {
+            if ($dataBase->ejecutar($consulta)) {
+                        $respuesta = true;
+            } else {
+                        throw new Exception("Error: la consulta no se pudo ejecutar");
+            }
+        } else {
+            throw new Exception("Error: la base de datos no se pudo iniciar");
+        }
+        return $respuesta;
+    }
+    // Funcion para modificar los datos de un responsable
+    // Return
+    public function modificar() {
+        $respuesta = false;
+        $dataBase = new DataBase();
+        $consulta = "UPDATE responsable SET numeroLicencia='". $this->getNumeroLicencia() . 
+                                            "',nombre='" . $this->getNombre() .          
+    }
 }
 ?>

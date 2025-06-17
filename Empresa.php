d8f230e4e51927a0b6e4e5fd44e45047b4b7eec6
Benjamin de la fuente <206235917+Ibenjamindlf@users.noreply.github.com>
2025-06-17 15:51:17 -0300

    empresa.php revisión gabi

--- Empresa.php
+++ Empresa.php
@@ -1,11 +1,8 @@
 <?php
 // Empresa.php inicio
 class Empresa{
-    // Atributo estático
-    private static int $idEmpresaStatic = 0;
-
     // Atributos de instacia
-    private int $idEmpresa;
+    private int $idEmpresa = 0;
     private string $nombre;
     private string $direccion;
 
@@ -14,8 +11,6 @@ class Empresa{
         string $nombre,
         string $direccion
     ){
-        self::$idEmpresaStatic++;// Incremento automático del código
-        $this->idEmpresa = self::$idEmpresaStatic;
         $this->nombre = $nombre;
         $this->direccion = $direccion;
     }
@@ -34,6 +29,10 @@ class Empresa{
     }
 
     // SETTERS
+    private function setIdEmpresa(int $idEmpresa): void{
+        $this->idEmpresa = $idEmpresa;
+    }
+
     public function setNombre(string $nombre): void{
         $this->nombre = $nombre;
     }
@@ -53,6 +52,165 @@ class Empresa{
         "Nombre: $nombre\n".
         "Dirección: $direccion\n".
         "-----------------------------";
+    }
+
+    // 5 funciones (buscar,listar,insertar,modificar,eliminar) -> phpMyAdmin
+    /**
+     * Función para buscar empresa segun idEmpresa.
+     * Retorna true si la encuentra, falso caso contrario.
+     * 
+     * @param int idEmpresa
+     * @return bool
+    */
+    public function buscar(int $idEmpresa): bool{
+        $dataBase = new DataBase();
+        $consulta = "SELECT * FROM empresa WHERE idEmpresa = '" . $idEmpresa . "'";
+        $respuesta = false;
+
+        if($dataBase->iniciar()) {
+            if($dataBase->ejecutar($consulta)){
+                // Mientras $fila tenga valor el if se ejecuta
+                if ($fila = $dataBase->registro()) {
+                    $this->setIdEmpresa($idEmpresa);
+                    $this->setNombre($fila['nombre']);
+                    $this->setDireccion($fila['direccion']);
+
+                    $respuesta = true;
+                }
+            }else{
+                throw new Exception ($dataBase->getError());
+            }
+        }else{
+            throw new Exception ($dataBase->getError());
+        }
+
+        return $respuesta;
+    }
+
+    /**
+     * Función para listar toda la tabla Empresa
+     * 
+     * @param string $condicion
+     * @return array
+    */
+    public static function listar(string $condicion): array{
+        $arrayEmpresa = [];
+        $dataBase = new DataBase();
+        $consulta = "SELECT * FROM empresa";
+
+        if($condicion != ""){
+            $consulta .= " WHERE " . $condicion;
+        }
+        $consulta .= " ORDER BY nombre";
+
+        if($dataBase->iniciar()) {
+            if($dataBase->ejecutar($consulta)){
+                // Mientras $fila tenga valor el if se ejecuta
+                while ($fila = $dataBase->registro()) {
+                    $objEmpresa = new Empresa(
+                        $fila['nombre'],
+                        $fila['direccion']
+                    );
+                    $objEmpresa->setIdEmpresa($fila['idEmpresa']);
+
+                    $arrayEmpresa[] = $objEmpresa;
+                }
+            }else{
+                throw new Exception ($dataBase->getError());
+            }
+        }else{
+            throw new Exception ($dataBase->getError());
+        }
+
+        return $arrayEmpresa;
+    }
+
+    /**
+     * Función para insertar registro de Empresa.
+     * Retorna true en caso de éxito
+     * 
+     * @return bool
+    */
+    public function insertar(): bool{
+        $dataBase = new DataBase();
+        $respuesta = false;
+        $consulta =
+            "INSERT INTO empresa(nombre, direccion)
+            VALUES ('".$this->getNombre()."', '".$this->getDireccion()."');"
+        ;
+
+
+        if($dataBase->iniciar()) {
+            $idInsertado = $dataBase->devuelveIDInsercion($consulta);
+            
+            if($idInsertado !== null){
+                $this->setIdEmpresa($idInsertado);
+                $respuesta = true;
+            }else{
+                throw new Exception ($dataBase->getError()); 
+            }
+        }else{
+            throw new Exception ($dataBase->getError());
+        }
+
+        return $respuesta;
+    }
+
+    /**
+     * Función para modificar datos de Empresa.
+     * Retorna true en caso de éxito
+     * 
+     * @return bool
+    */
+    public function modificar(): bool{
+        $respuesta = false;
+        $dataBase = new DataBase();
+        $consulta =
+            "UPDATE empresa 
+            SET nombre = '".$this->getNombre()."',
+            direccion = '".$this->getDireccion()."'
+            WHERE idEmpresa = ".$this->getIdEmpresa().";"
+        ;
+
+        if($dataBase->iniciar()) {
+            if($dataBase->ejecutar($consulta)){
+                $respuesta = true;
+            }else{
+                throw new Exception ($dataBase->getError()); 
+            }
+        }else{
+            throw new Exception ($dataBase->getError());
+        }
+
+        return $respuesta;
+    }
+
+    /**
+     * Función para eliminar registro de Empresa.
+     * Retorna true en caso de éxito
+     * 
+     * @return bool
+    */
+    public function eliminar(): bool{
+        $respuesta = false;
+        $dataBase = new DataBase();
+
+        if($dataBase->iniciar()) {
+            $consulta = 
+                "DELETE FROM empresa
+                WHERE idEmpresa = ".$this->getIdEmpresa().";"
+            ;
+
+            if($dataBase->ejecutar($consulta)){
+                $respuesta = true;
+            }else{
+                throw new Exception ($dataBase->getError()); 
+            }
+        }else{
+            throw new Exception($dataBase->getError());
+        }
+
+        return $respuesta;
     }
 }
 ?>
